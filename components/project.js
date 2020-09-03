13a4390449c557228c52d33f43471902237fe1e9
gad laviod <gadilabi@gmail.com>
2020-09-03 12:38:04 +0300

    Creating a project component.

--- components/project.js
+++ components/project.js
@@ -2,44 +2,140 @@ var templateProject = document.createElement("template");
 
 templateProject.innerHTML = `
 
-<style>
 	
-	#component{
-		width:100%;
-		height:100%;
-		background-color: #082a49;
-		
-	}
+	<style>
+		#card {
+			--color: #fe076e;
+			--card-width: 400px;
+			--img-width: var(--card-width);
+			--img-height: calc(var(--img-width) / 2.1);
+			font-family: arial;
+			width: 400px;
+			border: 2px solid black;
+			border-radius: 6px;
+			overflow: hidden;
 
-	#logo{
-		height: 80%;
-		display:grid;
-		place-items: center;	
-	}
+		}
 
-	#logo>img{
-		height:100%;
-	}
+		#img-container {
+			width: 100%;
+			height: var(--img-height);
+		}
 
-	@media(max-width: 800px){
-		
+		#img-container>img {
+			display: block;
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+		}
+
+		#header {
+			text-align: center;
+			color: var(--color);
+			margin: 20px;
+			font-size: 20px;
+			font-weight: bolder;
+		}
 
 
-	}
+		#description {
 
-</style>
+			margin: 40px;
+			color: rgb(50, 50, 50);
+		}
 
-<div id="component">
+		#links {
+			cursor: pointer;
+			background-color: var(--color);
+			display: flex;
+			justify-content: space-around;
+		}
 
-	<div id="logo">
-		<img src="images/logo.svg" alt="" />
+		#links>a {
+
+			all: unset;
+			color: white;
+
+
+		}
+
+		#technology {
+			display: flex;
+			width: 100%;
+			background-color: var(--color);
+			color: white;
+			border-top: 1px solid rgb(50, 50, 50);
+		}
+
+		#backend {
+			flex-basis: 50%;
+			text-align: center;
+			border-right: 1px solid rgb(50, 50, 50);
+
+		}
+
+		#frontend {
+			flex-basis: 50%;
+			text-align: center;
+		}
+
+		.tech-header {
+			font-weight: bolder;
+
+		}
+
+	</style>
+
+	<div id="card">
+
+		<div id="img-container">
+			<img src="images/neteacher.png" alt="">
+
+		</div>
+
+		<div id="header">
+
+			<div id="project-num">
+
+				
+
+			</div>
+			<div id="project-name" style="margin-top:10px; color:black;">Job Search Platform</div>
+
+		</div>
+		<div id="description">
+
+			This is a job search platform. It allows users to register as job seekers or as recruiters. Job seekers can upload their cv, search for jobs and apply with one click. Recruiters can publish ads, track the number of applicants and download their cvs.
+		</div>
+
+
+		<div id="links">
+			<a id="live-site" href="https://avodatova.co.il">
+				Live Site
+			</a>
+
+		</div>
+		<div id="technology">
+			<div id="backend">
+
+				<div class="tech-header">Backend</div>
+
+				<div>PHP</div>
+
+			</div>
+			<div id="frontend">
+
+				<div class="tech-header">Frontend</div>
+
+
+				Vanilla js
+
+
+			</div>
+
+		</div>
 
 	</div>
-
-
-	
-
-</div>
 
 `;
 
@@ -55,13 +151,42 @@ class Project extends HTMLElement {
 
 		this.shadowRoot.appendChild(templateProject.content.cloneNode(true));
 
+		this.imgUrl = this.getAttribute("img-url");
+
+		this.img = this.shadowRoot.querySelector("img");
+
+		this.projectNumber = this.getAttribute("project-number");
+
+		this.projectNumberContainer = this.shadowRoot.querySelector("#project-num");
+
+		this.projectName = this.getAttribute("project-name");
+
+		this.projectNameContainer = this.shadowRoot.querySelector("#project-name");
+
+		this.liveUrl = this.getAttribute('live-site');
+
+		this.liveSite = this.shadowRoot.querySelector("#live-site");
+
+		this.frontendTech = this.getAttribute('frontend');
+
+		this.frontendContainer = this.shadowRoot.querySelector("#frontend");
+
+		this.backendTech = this.getAttribute('backend');
+
+
+		this.backendContainer = this.shadowRoot.querySelector("#backend");
+
+		this.embedAttributes();
+
+	}
+
+	embedAttributes() {
 
 
 	}
 
 	connectedCallback() {
 
-		const that = this;
 
 
 
