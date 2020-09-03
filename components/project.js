c2d6ebc2d47f2b87582bce3a33a7f41b9bd492b8
gad laviod <gadilabi@gmail.com>
2020-09-03 12:57:07 +0300

    1) Created a project component

--- components/project.js
+++ components/project.js
@@ -149,45 +149,194 @@ class Project extends HTMLElement {
 			mode: 'open'
 		});
 
-		this.shadowRoot.appendChild(templateProject.content.cloneNode(true));
 
-		this.imgUrl = this.getAttribute("img-url");
-
-		this.img = this.shadowRoot.querySelector("img");
-
-		this.projectNumber = this.getAttribute("project-number");
-
-		this.projectNumberContainer = this.shadowRoot.querySelector("#project-num");
-
-		this.projectName = this.getAttribute("project-name");
-
-		this.projectNameContainer = this.shadowRoot.querySelector("#project-name");
-
-		this.liveUrl = this.getAttribute('live-site');
-
-		this.liveSite = this.shadowRoot.querySelector("#live-site");
-
-		this.frontendTech = this.getAttribute('frontend');
-
-		this.frontendContainer = this.shadowRoot.querySelector("#frontend");
-
-		this.backendTech = this.getAttribute('backend');
+		this.template = document.createElement('template');
 
 
-		this.backendContainer = this.shadowRoot.querySelector("#backend");
 
-		this.embedAttributes();
+		//		this.img = this.shadowRoot.querySelector("img");
+		//
+		//
+		//		this.projectNumberContainer = this.shadowRoot.querySelector("#project-num");
+		//
+		//
+		//		this.projectNameContainer = this.shadowRoot.querySelector("#project-name");
+		//
+		//
+		//		this.liveSite = this.shadowRoot.querySelector("#live-site");
+
+
+
+
+
+		//		this.backendContainer = this.shadowRoot.querySelector("#backend");
+
 
 	}
 
 	embedAttributes() {
+
+		this.template.innerHTML = `
+
+	<style>
+		#card {
+			--color: #fe076e;
+			--card-width: 400px;
+			--img-width: var(--card-width);
+			--img-height: calc(var(--img-width) / 2.1);
+			font-family: arial;
+			width: 400px;
+			border: 2px solid black;
+			border-radius: 6px;
+			overflow: hidden;
+
+		}
+
+		#img-container {
+			width: 100%;
+			height: var(--img-height);
+		}
+
+		#img-container>img {
+			display: block;
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+		}
+
+		#header {
+			text-align: center;
+			color: var(--color);
+			margin: 20px;
+			font-size: 20px;
+			font-weight: bolder;
+		}
+
+
+		#description {
+
+			margin: 40px;
+			color: rgb(50, 50, 50);
+		}
+
+		#links {
+			cursor: pointer;
+			background-color: var(--color);
+			display: flex;
+			justify-content: space-around;
+		}
+
+		#links>a {
+
+			all: unset;
+			color: white;
+
+
+		}
+
+		#technology {
+			display: flex;
+			width: 100%;
+			background-color: var(--color);
+			color: white;
+			border-top: 1px solid rgb(50, 50, 50);
+		}
+
+		#backend {
+			flex-basis: 50%;
+			text-align: center;
+			border-right: 1px solid rgb(50, 50, 50);
+
+		}
+
+		#frontend {
+			flex-basis: 50%;
+			text-align: center;
+		}
+
+		.tech-header {
+			font-weight: bolder;
+
+		}
+
+	</style>
+
+	<div id="card">
+
+		<div id="img-container">
+			<img src="${this.imgUrl}" alt="">
+
+		</div>
+
+		<div id="header">
+
+			<div id="project-num">
+
+				${this.projectNumber}
+				
+
+			</div>
+			<div id="project-name" style="margin-top:10px; color:black;">
+
+				${this.projectName}
+
+			</div>
+
+		</div>
+		<div id="description">
+				
+			${this.description}
+
+		</div>
+
+
+		<div id="links">
+			<a id="live-site" href="${this.liveUrl}">
+				Live Site
+			</a>
+
+		</div>
+		<div id="technology">
+			<div id="backend">
+
+				<div class="tech-header">Backend</div>
+
+				<div>${this.backendTech}</div>
+
+			</div>
+			<div id="frontend">
+
+				<div class="tech-header">Frontend</div>
+
+
+				<div>
+					${this.frontendTech}
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+
+		`;
+		this.shadowRoot.appendChild(this.template.content.cloneNode(true));
 
 
 	}
 
 	connectedCallback() {
 
-
+		this.imgUrl = this.getAttribute("img-url");
+		this.projectNumber = this.getAttribute("project-number");
+		this.projectName = this.getAttribute("project-name");
+		this.liveUrl = this.getAttribute('live-site');
+		this.frontendTech = this.getAttribute('frontend');
+		this.backendTech = this.getAttribute('backend');
+		this.description = this.getAttribute("description");
+		this.embedAttributes();
 
 
 	}
